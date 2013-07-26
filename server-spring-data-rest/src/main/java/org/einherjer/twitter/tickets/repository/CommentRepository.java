3752aa68689a867d1c73cd792aa35a03478814ce
einherjer-uy <andres.sica@gmail.com>
2013-07-26 17:06:11 -0300

    basic cruds

--- server-spring-data-rest/src/main/java/org/einherjer/twitter/tickets/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package org.einherjer.twitter.tickets.repository;
-
-import org.einherjer.twitter.tickets.model.Comment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-
-}
