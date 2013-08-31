0af7008e7ab36b5b446d5e94a937cfe39b1e002b
einherjer-uy <andres.sica@gmail.com>
2013-08-31 19:01:46 -0300

    highlight approved rejected tickets

--- src/main/webapp/public/js/views/ticketRow.js
+++ src/main/webapp/public/js/views/ticketRow.js
@@ -17,13 +17,26 @@ var app = app || {};
 
 		render: function () {
 			this.$el.html(this.template(this.model.toJSON()));
+			
 			this.$(".actions").append(new app.ActionsView({ model: this.model, onAddEditModal: false, $messages: this.$messages }).render().el);
-			if (this.model.get("unread")) {
-				this.$el.addClass("unread");
+
+			if (app.loggedUser.role==app.util.ROLE_REQUESTOR) {
+				if (this.model.get("status")=="APPROVED") {
+					this.$el.addClass("success");
+				}
+				if (this.model.get("status")=="REJECTED") {
+					this.$el.addClass("error");
+				}
 			}
 			else {
-				this.$el.removeClass("unread");
+				if (this.model.get("unread")) {
+					this.$el.addClass("unread");
+				}
+				else {
+					this.$el.removeClass("unread");
+				}
 			}
+
 			return this;
 		}
 
