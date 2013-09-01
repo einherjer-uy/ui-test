a18cc9f89070c9d14b61be24cb64355c28bcf110
Matias Gelos <matiasgelos@Matias-MacBook-Pro.local>
2013-09-01 11:01:25 -0300

    Added remember selected dashboard tab (storing in server)

--- src/main/webapp/public/js/app.js
+++ src/main/webapp/public/js/app.js
@@ -5,10 +5,6 @@ $(function () { "use strict";  //$() shorthand for $(document).ready()
 	app.util.getDropdownInfo("/tt/ticketTypes", app.ticketTypes);
 	app.ticketPriorities = [];
 	app.util.getDropdownInfo("/tt/ticketPriorities", app.ticketPriorities);
-	app.loggedUser = {};
-	$.getJSON("/tt/loggedUser", function(result) {
-		app.loggedUser = result;
-	});
 
 	new app.AppView();
 	new app.DashoardListView();
